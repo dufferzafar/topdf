4e03bcaf3ea7e62794cdbcda5cb263c63577e468
Shadab Zafar <dufferzafar0@gmail.com>
2016-01-11 00:36:12 +0530

    Add PandocEngine

---
+++ topdf/pdf_engines/pandoc.py
@@ -0,0 +1,28 @@
+
+import os
+import pypandoc
+
+_formats = ["docx", "epub", "html", "json",
+            "tex", "md", "opml", "rst"]
+
+
+class PandocEngine():
+
+    """ Uses pandoc to convert . """
+
+    @staticmethod
+    def can_handle(file_ext):
+        return file_ext[1:] in _formats
+
+    @staticmethod
+    def make_pdf(path):
+        file_name, file_ext = os.path.splitext(os.path.basename(path))
+        pypandoc.convert(
+            path,
+            to='pdf',
+            outputfile=file_name + ".pdf",
+            extra_args=[
+                "--toc",
+                "--latex-engine", "xelatex",
+            ]
+        )
