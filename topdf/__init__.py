464f7a490f66cec191826762b1f5bcfefb5ff2c7
Shadab Zafar <dufferzafar0@gmail.com>
2016-01-11 01:07:35 +0530

    Option to open output pdf in associated viewer

--- topdf/__init__.py
+++ topdf/__init__.py
@@ -1,7 +1,7 @@
 from topdf.path_handlers import list_handlers
 
 
-def topdf(path):
+def topdf(path, open_pdf=False):
     handler = [hnd for name, hnd in list_handlers().items()
                if hnd.can_handle(path)]
 
@@ -9,4 +9,14 @@ def topdf(path):
         raise RuntimeError("No appropriate handler could be found"
                            " for path: %s" % path)
 
-    handler[0].make_pdf(path)
+    pdf = handler[0].make_pdf(path)
+
+    # Open the PDF file in associated viewer
+    if open_pdf:
+        import sys
+        if sys.platform == 'linux':
+            import subprocess
+            subprocess.call(["xdg-open", pdf])
+        else:
+            import os
+            os.startfile(pdf)
