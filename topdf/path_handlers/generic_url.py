5dbfaeb866af944ef278088e9460d0d5b435b9af
Shadab Zafar <dufferzafar0@gmail.com>
2016-09-01 01:42:46 +0530

    Add a generic URL handler that just calls pandoc

---
+++ topdf/path_handlers/generic_url.py
@@ -0,0 +1,22 @@
+import urllib.parse
+
+from topdf.path_handlers.base import BaseHandler
+from topdf.pdf_engines.pandoc import PandocEngine
+
+
+class GenericURL(BaseHandler):
+
+    """ Handles any arbitrary URL. """
+
+    # Minimum priority so that all other handlers get matched first.
+    priority = 0
+
+    @classmethod
+    def can_handle(cls, path):
+        parsed = urllib.parse.urlsplit(path)
+        print(parsed)
+        return parsed.scheme.startswith("http")
+
+    @classmethod
+    def make_pdf(cls, path):
+        return PandocEngine.make_pdf(path)
